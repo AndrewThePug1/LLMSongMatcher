457713a8948ee85da6d73b1adbf117247438e754
AndrewThePug1 <andrewmondejarp@gmail.com>
2024-02-20 12:02:35 -0500

    Added requirements.txt with project dependencies

--- test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Print the current working directory
-print("Current Working Directory:", os.getcwd())
-
-# Update this path according to the actual location of the 'ChurchJSONFiles' directory relative to the current working directory
-directory_path = 'ChurchJSONFiles'
